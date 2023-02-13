df77a3b56973994f3cdcc01f95b3bb33ea8caff8
Theresa Wiegert <theresa.wiegert@gmail.com>
2023-02-13 14:49:06 +0100

    added multiplication

--- mathy_things.py
+++ mathy_things.py
@@ -3,3 +3,7 @@ def summation(a,b):
 
 def difference (a,b):
 	return a-b
+
+# Homework: create a function that multiplies 2 things. Add , commit with good commit message, and push it to your fork. 
+def multiplication (a,b):
+	return a*b
