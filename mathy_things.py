1109b5b621783281e722b7752feb4c8ef5e58d0d
Theresa Wiegert <theresa.wiegert@gmail.com>
2023-02-14 23:30:24 +0100

    added test message

--- mathy_things.py
+++ mathy_things.py
@@ -7,3 +7,5 @@ def difference (a,b):
 # Homework: create a function that multiplies 2 things. Add , commit with good commit message, and push it to your fork. 
 def multiplication (a,b):
 	return a*b
+
+# adding this test message to see if it gets updated on the hub... 
