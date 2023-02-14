f5c6c732e9c354c0bd3e906c65b91b9f4db7e02c
Theresa Wiegert <theresa.wiegert@gmail.com>
2023-02-14 23:22:34 +0100

    added content

--- mathy_things.py
+++ mathy_things.py
@@ -5,5 +5,5 @@ def difference (a,b):
 	return a-b
 
 # Homework: create a function that multiplies 2 things. Add , commit with good commit message, and push it to your fork. 
-def multiplication (a,b):
-	return a*b
+# def multiplication (a,b):
+#	return a*b
