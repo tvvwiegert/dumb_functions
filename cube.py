9a91f2848fb0930979f3a7987b7d0c1423d4d013
Clara <claracd726@gmail.com>
2023-02-15 09:43:42 +0100

    Added a cuby function!

--- cube.py
+++ cube.py
@@ -1,10 +1,10 @@
-#Cube of a number
+#Cube of a number. With some changes to commit
 while True:
     try:
-        nbr = float(input("Now enter a number: "))       
+        nbr = float(input("Now enter a number: "))
     except ValueError:
         print("Have a look at this: https://en.wikipedia.org/wiki/Number")
         continue
     else:
         print ("The cube of ", nbr, "is", nbr*nbr*nbr)
-        break 
+        break
